3eba6c420b1051f806a4526c1bc8cb57abb74851
fsprogamer <fsprogamer@gmail.com>
2016-12-09 16:21:34 +0300

    +

---
+++ MongoDBApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+
+namespace MongoDBApp
+{
+    class Program
+    {
+        protected static IMongoClient _client;
+        protected static IMongoDatabase _database;
+
+        private static void PrintMenu()
+        {
+            Console.WriteLine("p \t список всех пользователей");
+            Console.WriteLine("a \t добавить пользователя");
+            Console.WriteLine("r \t удалить пользователя");
+            Console.WriteLine("m \t отобразить меню");
+            Console.WriteLine("e \t выход\n");
+        }
+
+       
+        static void Main()
+        {
+            const string connect = "mongodb://localhost";
+            _client = new MongoClient(connect);          
+            _database = _client.GetDatabase("test");
+
+            Info info = new Info();
+
+            //info.Insert(_database);
+            info.Select(_database);          
+            
+        }
+    }
+}
---
+++ MongoDBApp/User.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace MongoDBApp
+{
+    public class User
+    {
+        public ObjectId Id { get; set; }
+        public String Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class Info
+    {
+        async public void Insert(IMongoDatabase _database)
+        {
+            var document = new BsonDocument
+            {
+                { "address" , new BsonDocument
+                    {
+                        { "street", "2 Avenue" },
+                        { "zipcode", "10075" },
+                        { "building", "1480" },
+                        { "coord", new BsonArray { 73.9557413, 40.7720266 } }
+                    }
+                },
+                { "borough", "Manhattan" },
+                { "cuisine", "Italian" },
+                { "grades", new BsonArray
+                    {
+                        new BsonDocument
+                        {
+                            { "date", new DateTime(2014, 10, 1, 0, 0, 0, DateTimeKind.Utc) },
+                            { "grade", "A" },
+                            { "score", 11 }
+                        },
+                        new BsonDocument
+                        {
+                            { "date", new DateTime(2014, 1, 6, 0, 0, 0, DateTimeKind.Utc) },
+                            { "grade", "B" },
+                            { "score", 17 }
+                        }
+                    }
+                },
+                { "name", "Vella" },
+                { "restaurant_id", "41704620" }
+            };
+
+            var collection = _database.GetCollection<BsonDocument>("restaurants");
+            await collection.InsertOneAsync(document);
+        }
+
+        async public void Select(IMongoDatabase _database)
+        {
+            var collection = _database.GetCollection<BsonDocument>("restaurants");
+            var filter = new BsonDocument();
+            var count = 0;
+            //using (var cursor = collection.Find(filter))
+            {
+                //    while (await cursor.MoveNextAsync())
+                //    {
+                //        var batch = cursor.Current;
+                //        foreach (var document in batch)
+                //        {
+                //            // process document
+                //            count++;
+                //        }
+                //    }
+                //}
+
+                var restaurants = collection.FindSync(filter).ToList();
+                foreach (var restaurant in restaurants)
+                {
+                    // Console.WriteLine("Имя: {0}, Возраст: {1}", restaurant, restaurant.Age);
+                }
+            }
+        }
+    }
+}
